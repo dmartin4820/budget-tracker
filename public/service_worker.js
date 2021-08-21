75fdef226370b31050e75f070f1f4672e951bb9d
Dmartin <dmartin4820@yahoo.comgit config --global user.email dmartin4822@yahoo.com>
2021-08-21 13:48:15 -0700

    added code to handle POST requests and GET requests to api/transaction

--- public/service_worker.js
+++ public/service_worker.js
@@ -1,11 +1,13 @@
 const CACHE_NAME = 'budget_tracker_cache_1';
-const DATABASE_CACHE = "budget_tracker_db_cache_1";
+const RUNTIME_CACHE = "budget_tracker_runtime_cache_1";
 
 const FILES_TO_CACHE = [
 	'/',
-	'/index.html',
-	'/styles.css',
-	'/index.js'
+	'./index.html',
+	'./styles.css',
+	'./index.js',
+	'./icons/icon-192x192.png',
+	'./icons/icon-512x512.png'
 ]
 
 self.addEventListener('install', e => {
@@ -25,7 +27,7 @@ self.addEventListener('activate', e => {
 		const cacheList = await caches.keys();
 
 		//Delete caches that we aren't currently using
-		const oldCacheList = cacheList.filter(cache => cache !== CACHE_NAME && cache !== DATABASE_CACHE);
+		const oldCacheList = cacheList.filter(cache => cache !== CACHE_NAME && cache !== RUNTIME_CACHE);
 		await Promise.all(oldCacheList.map(cache => caches.delete(cache)));
 		return self.clients.claim();
 	}
@@ -37,18 +39,37 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
 
-	//Check if the response to the request is cached, then return the cached response.
-	//If no cached response, open the database cache and do a fetch request normally and cache the cloned response.
+	//For requests other than GET, do a normal fetch which will be stored in indexedDB.
 	const response = async () => {
-		const cachedResponse = await caches.match(e.request);
-	
-		console.log(cachedResponse)
+		// console.log(cachedResponse)
+		if (e.request.method !== "GET") {
+			console.log('POST')
+			return fetch(e.request)
+		}
 
+		//If online, fetch from server to keep data on the page updated
+		if (e.request.url.includes('/api/transaction')) {
+			const cache = await caches.open(RUNTIME_CACHE);
+			try {
+				const fetchedResponse = await fetch(e.request);
+				cache.put(e.request, fetchedResponse.clone());
+
+				return fetchedResponse;
+
+			} catch(err) {
+				return caches.match(e.request);
+			}
+		}
+
+	
+		//Check if the response to the request is cached, then return the cached response.
+		const cachedResponse = await caches.match(e.request);
 		if (cachedResponse) {
 			return cachedResponse;
 		}
 
-		const cache = await caches.open(DATABASE_CACHE);
+		//If no cached response, open the database cache and do a fetch request normally and cache the cloned response.
+		const cache = await caches.open(RUNTIME_CACHE);
 		const fetchedResponse = await	fetch(e.request);
 		
 		await cache.put(e.request, fetchedResponse.clone());
