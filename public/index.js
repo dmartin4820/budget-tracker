3e2e09c583ff5a236988e79a10be6c075df1739c
Dmartin <dmartin4820@yahoo.comgit config --global user.email dmartin4822@yahoo.com>
2021-08-21 20:42:20 -0700

    reformatted code

--- public/index.js
+++ public/index.js
@@ -1,44 +1,44 @@
 let transactions = [];
 let myChart;
 
-
 //Open database and setup transaction store
-console.log('opening indexedDB')
+console.log('opening indexedDB');
 const request = window.indexedDB.open('budgetTracker', 1);
 
 //If the user reopens the page, and has reconnected to the internet
 //populate the server DB and clear the client DB
 if (navigator.onLine) {
-	request.onsuccess = e => {
-		sendRecordsToServer();
-		clearTransactionDB();
-	}
+  request.onsuccess = () => {
+    sendRecordsToServer();
+    clearTransactionDB();
+    populateChart();
+    populateTable();
+    populateTotal();
+  };
 }
 
+request.onupgradeneeded = () => {
+  const db = request.result;
+  db.createObjectStore('transaction', { keyPath: 'date' });
+};
 
-request.onupgradeneeded = e => {
-	const db = request.result;
-	db.createObjectStore('transaction', {keyPath: 'date'});
-}
-
-fetch("/api/transaction")
-  .then(response => {
-		console.log('fetch')
+fetch('/api/transaction')
+  .then((response) => {
     return response.json();
   })
-  .then(data => {
+  .then((data) => {
     // save db data on global variable
     transactions = data;
 
-		//If user is offline, populate the chart with data from the client DB
-		if (!navigator.onLine) {
-			console.log('populate offline transactions to chart')
-			getOfflineTransactions()
-		}	else {	
-    		populateTotal();
-    		populateTable();
-    		populateChart();
-		}
+    //If user is offline, populate the chart with data from the client DB
+    if (!navigator.onLine) {
+      console.log('populate offline transactions to chart');
+      getOfflineTransactions();
+    } else {
+      populateTotal();
+      populateTable();
+      populateChart();
+    }
   });
 
 function populateTotal() {
@@ -47,17 +47,17 @@ function populateTotal() {
     return total + parseInt(t.value);
   }, 0);
 
-  let totalEl = document.querySelector("#total");
+  let totalEl = document.querySelector('#total');
   totalEl.textContent = total;
 }
 
 function populateTable() {
-  let tbody = document.querySelector("#tbody");
-  tbody.innerHTML = "";
+  let tbody = document.querySelector('#tbody');
+  tbody.innerHTML = '';
 
-  transactions.forEach(transaction => {
+  transactions.forEach((transaction) => {
     // create and populate a table row
-    let tr = document.createElement("tr");
+    let tr = document.createElement('tr');
     tr.innerHTML = `
       <td>${transaction.name}</td>
       <td>${transaction.value}</td>
@@ -73,13 +73,13 @@ function populateChart() {
   let sum = 0;
 
   // create date labels for chart
-  let labels = reversed.map(t => {
+  let labels = reversed.map((t) => {
     let date = new Date(t.date);
     return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
   });
 
   // create incremental values for chart
-  let data = reversed.map(t => {
+  let data = reversed.map((t) => {
     sum += parseInt(t.value);
     return sum;
   });
@@ -89,125 +89,126 @@ function populateChart() {
     myChart.destroy();
   }
 
-  let ctx = document.getElementById("myChart").getContext("2d");
+  let ctx = document.getElementById('myChart').getContext('2d');
 
   myChart = new Chart(ctx, {
     type: 'line',
-      data: {
-        labels,
-        datasets: [{
-            label: "Total Over Time",
-            fill: true,
-            backgroundColor: "#6666ff",
-            data
-        }]
-    }
+    data: {
+      labels,
+      datasets: [
+        {
+          label: 'Total Over Time',
+          fill: true,
+          backgroundColor: '#6666ff',
+          data,
+        },
+      ],
+    },
   });
 }
 
 async function getOfflineTransactions() {
-	const db = request.result;
-	const transaction = db.transaction(['transaction'], 'readwrite');
-	const budgetTrackerStore = transaction.objectStore('transaction');
+  const db = request.result;
+  const transaction = db.transaction(['transaction'], 'readwrite');
+  const budgetTrackerStore = transaction.objectStore('transaction');
 
-	const allTransactions = budgetTrackerStore.getAll();
+  const allTransactions = budgetTrackerStore.getAll();
 
-	allTransactions.onsuccess = e => {
-		let transactionsArr = allTransactions.result;
-		let placeHolderId = 0;
+  allTransactions.onsuccess = () => {
+    let transactionsArr = allTransactions.result;
+    let placeHolderId = 0;
 
-		transactionsArr.forEach(t => {
-			t._id = String(placeHolderId++);
-			t.__v = 0;
+    transactionsArr.forEach((t) => {
+      t._id = String(placeHolderId++);
+      t.__v = 0;
 
-			transactions.unshift(t);
-		})
+      transactions.unshift(t);
+    });
 
-		populateTotal();
+    populateTotal();
     populateTable();
     populateChart();
-	}
+  };
 }
 
 //Clear the database once the user is back online
 function clearTransactionDB() {
-	console.log('clear db')
-	const db = request.result;
-	const transaction = db.transaction(['transaction'], 'readwrite');
-	const budgetTrackerStore = transaction.objectStore('transaction');
+  console.log('clear db');
+  const db = request.result;
+  const transaction = db.transaction(['transaction'], 'readwrite');
+  const budgetTrackerStore = transaction.objectStore('transaction');
 
-	budgetTrackerStore.clear().onsuccess = e => {
-		console.log('db cleared')
-	}
+  budgetTrackerStore.clear().onsuccess = () => {
+    console.log('db cleared');
+  };
 }
 
 //Send all the transactions to the server once the user is back online
 function sendRecordsToServer() {
-	console.log('send to server')
-	const db = request.result;
-	const transaction = db.transaction(['transaction'], 'readwrite');
-	const budgetTrackerStore = transaction.objectStore('transaction');
+  console.log('send to server');
+  const db = request.result;
+  const transaction = db.transaction(['transaction'], 'readwrite');
+  const budgetTrackerStore = transaction.objectStore('transaction');
 
-	const allTransactions = budgetTrackerStore.getAll();
+  const allTransactions = budgetTrackerStore.getAll();
 
-	// console.log(allTransactions)
+  // console.log(allTransactions)
 
-	allTransactions.onsuccess = async e => {
-		try {
-			if (allTransactions.result.length !== 0) { 
-				const response = await fetch("/api/transaction/bulk", {
-  			  method: "POST",
-  			  body: JSON.stringify(allTransactions.result),
-  			  headers: {
-  			    Accept: "application/json, text/plain, */*",
-  			    "Content-Type": "application/json"
-  			  }
-  			});
+  allTransactions.onsuccess = async () => {
+    try {
+      if (allTransactions.result.length !== 0) {
+        const response = await fetch('/api/transaction/bulk', {
+          method: 'POST',
+          body: JSON.stringify(allTransactions.result),
+          headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json',
+          },
+        });
 
-				if (response.ok) {
-					console.log('transactions saved')
-				} else {//catch any status code other than ok
-					console.log(await response.json())//print err
-				}
-			} else {
-				console.log('Nothing to store')
-			}
-
-		} catch(err) {
-			console.log(err)
-		}
-	}
+        if (response.ok) {
+          console.log('transactions saved');
+        } else {
+          //catch any status code other than ok
+          console.log(await response.json()); //print err
+        }
+      } else {
+        console.log('Nothing to store from client DB');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 }
 
 //saveRecord takes in data while the user is offline and stores it in an
 //the client DB
 function saveRecord(data) {
-	const db = request.result;
-	const transaction = db.transaction(['transaction'], 'readwrite');
-	const budgetTrackerStore = transaction.objectStore('transaction');
+  const db = request.result;
+  const transaction = db.transaction(['transaction'], 'readwrite');
+  const budgetTrackerStore = transaction.objectStore('transaction');
 
-	budgetTrackerStore.add(data);
+  budgetTrackerStore.add(data);
 }
 
 function sendTransaction(isAdding) {
-  let nameEl = document.querySelector("#t-name");
-  let amountEl = document.querySelector("#t-amount");
-  let errorEl = document.querySelector(".form .error");
+  let nameEl = document.querySelector('#t-name');
+  let amountEl = document.querySelector('#t-amount');
+  let errorEl = document.querySelector('.form .error');
 
   // validate form
-  if (nameEl.value === "" || amountEl.value === "") {
-    errorEl.textContent = "Missing Information";
+  if (nameEl.value === '' || amountEl.value === '') {
+    errorEl.textContent = 'Missing Information';
     return;
-  }
-  else {
-    errorEl.textContent = "";
+  } else {
+    errorEl.textContent = '';
   }
 
   // create record
   let transaction = {
     name: nameEl.value,
     value: amountEl.value,
-    date: new Date().toISOString()
+    date: new Date().toISOString(),
   };
 
   // if subtracting funds, convert amount to negative number
@@ -222,51 +223,49 @@ function sendTransaction(isAdding) {
   populateChart();
   populateTable();
   populateTotal();
-  
+
   // also send to server
-  fetch("/api/transaction", {
-    method: "POST",
+  fetch('/api/transaction', {
+    method: 'POST',
     body: JSON.stringify(transaction),
     headers: {
-      Accept: "application/json, text/plain, */*",
-      "Content-Type": "application/json"
-    }
+      Accept: 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+    },
   })
-  .then(response => {    
-    return response.json();
-  })
-  .then(data => {
-    if (data.errors) {
-      errorEl.textContent = "Missing Information";
-    }
-    else {
-      // clear form
-      nameEl.value = "";
-      amountEl.value = "";
-    }
-  })
-  .catch(err => {
-    // fetch failed, so save in indexed db
-    saveRecord(transaction);
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      if (data.errors) {
+        errorEl.textContent = 'Missing Information';
+      } else {
+        // clear form
+        nameEl.value = '';
+        amountEl.value = '';
+      }
+    })
+    .catch(() => {
+      // fetch failed, so save in indexed db
+      saveRecord(transaction);
 
-    // clear form
-    nameEl.value = "";
-    amountEl.value = "";
-  });
+      // clear form
+      nameEl.value = '';
+      amountEl.value = '';
+    });
 }
 
-
-document.querySelector("#add-btn").onclick = function() {
+document.querySelector('#add-btn').onclick = function () {
   sendTransaction(true);
 };
 
-document.querySelector("#sub-btn").onclick = function() {
+document.querySelector('#sub-btn').onclick = function () {
   sendTransaction(false);
 };
 
 //Check when the user comes back online then send all data to server
-window.addEventListener('online', e => {
-	console.log('online')
-	sendRecordsToServer();
-	clearTransactionDB();
+window.addEventListener('online', () => {
+  console.log('online');
+  sendRecordsToServer();
+  clearTransactionDB();
 });
--- public/service_worker.js
+++ public/service_worker.js
@@ -1,85 +1,85 @@
-const CACHE_NAME = 'budget_tracker_cache_3';
-const RUNTIME_CACHE = "budget_tracker_runtime_cache_3";
+const CACHE_NAME = 'budget_tracker_cache_1';
+const RUNTIME_CACHE = 'budget_tracker_runtime_cache_1';
 
 const FILES_TO_CACHE = [
-	'/',
-	'/index.html',
-	'/styles.css',
-	'/index.js',
-	'/icons/icon-192x192.png',
-	'/icons/icon-512x512.png'
-]
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/index.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
 
 self.addEventListener('install', e => {
-	//Create static files cache and store the static files
-	const install = async () => {
-		const cache = await caches.open(CACHE_NAME);
-		await cache.addAll(FILES_TO_CACHE);
-		return self.skipWaiting();
-	}
+  //Create static files cache and store the static files
+  const install = async () => {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll(FILES_TO_CACHE);
+    return self.skipWaiting();
+  };
 
-	e.waitUntil(install());
-})
+  e.waitUntil(install());
+});
 
 self.addEventListener('activate', e => {
 
-	const activate = async () => {
-		const cacheList = await caches.keys();
+  const activate = async () => {
+    const cacheList = await caches.keys();
 
-		//Delete caches that we aren't currently using
-		const oldCacheList = cacheList.filter(cache => cache !== CACHE_NAME && cache !== RUNTIME_CACHE);
-		await Promise.all(oldCacheList.map(cache => caches.delete(cache)));
-		return self.clients.claim();
-	}
+    //Delete caches that we aren't currently using
+    const oldCacheList = cacheList.filter(cache => cache !== CACHE_NAME && cache !== RUNTIME_CACHE);
+    await Promise.all(oldCacheList.map(cache => caches.delete(cache)));
+    return self.clients.claim();
+  };
 
-	e.waitUntil(activate());
-})
+  e.waitUntil(activate());
+});
 
 
 
 self.addEventListener('fetch', e => {
 
-	//For requests other than GET, do a normal fetch which will be stored in indexedDB.
-	const response = async () => {
-		// console.log(cachedResponse)
-		if (e.request.method !== "GET") {
-			console.log('POST')
-			return fetch(e.request)
-		}
+  //For requests other than GET, do a normal fetch which will be stored in indexedDB.
+  const response = async () => {
+    // console.log(cachedResponse)
+    if (e.request.method !== 'GET') {
+      console.log('POST');
+      return fetch(e.request);
+    }
 
-		//If online, fetch from server to keep data on the page updated
-		if (e.request.url.includes('/api/transaction')) {
-			const cache = await caches.open(RUNTIME_CACHE);
-			try {
-				const fetchedResponse = await fetch(e.request);
-				cache.put(e.request, fetchedResponse.clone());
+    //If online, fetch from server to keep data on the page updated
+    if (e.request.url.includes('/api/transaction')) {
+      const cache = await caches.open(RUNTIME_CACHE);
+      try {
+        const fetchedResponse = await fetch(e.request);
+        cache.put(e.request, fetchedResponse.clone());
 
-				return fetchedResponse;
+        return fetchedResponse;
 
-			} catch(err) {
-				return caches.match(e.request);
-			}
-		}
+      } catch(err) {
+        return caches.match(e.request);
+      }
+    }
 
-	
-		//Check if the response to the request is cached, then return the cached response.
-		const cachedResponse = await caches.match(e.request);
-		if (cachedResponse) {
-			return cachedResponse;
-		}
 
-		//If no cached response, open the database cache and do a fetch request normally and cache the cloned response.
-		const cache = await caches.open(RUNTIME_CACHE);
-		const fetchedResponse = await	fetch(e.request);
-		
-		await cache.put(e.request, fetchedResponse.clone());
+    //Check if the response to the request is cached, then return the cached response.
+    const cachedResponse = await caches.match(e.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    }
 
-		return fetchedResponse;
-	}
+    //If no cached response, open the database cache and do a fetch request normally and cache the cloned response.
+    const cache = await caches.open(RUNTIME_CACHE);
+    const fetchedResponse = await	fetch(e.request);
 
-	e.respondWith(response());
+    await cache.put(e.request, fetchedResponse.clone());
 
-})
+    return fetchedResponse;
+  };
+
+  e.respondWith(response());
+
+});
 
 
 
